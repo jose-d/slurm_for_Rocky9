fb641e0b1eab5077fdf8bdbe58d615c79f3f6589
jose_d <jose-d@users.noreply.github.com>
2025-03-26 18:08:53 +0100

    Update build_pmix.sh

--- build_pmix.sh
+++ build_pmix.sh
@@ -6,6 +6,9 @@ echo "PMIX_RELTAG: ${PMIX_RELTAG}, PMIX_VERSION: ${PMIX_VERSION}"
 # enable shell debug
 set -x
 
+# stop on any error
+set -e
+
 # install deps
 # N/A
 
@@ -19,7 +22,7 @@ rpm -qa | sort > "${GITHUB_WORKSPACE}/image_pmix_rpms.txt"
 # do rpmbuild
 rpmbuild --define 'build_all_in_one_rpm 0' \
          --define 'configure_options --disable-per-user-config-files' \
-         -ba ./pmix-*/contrib/pmix.spec &> ${GITHUB_WORKSPACE}/pmix_build.log
+         -ba ./pmix-*/contrib/pmix.spec
 
 mkdir "${GITHUB_WORKSPACE}/rpms"
 cp ${HOME}/rpmbuild/RPMS/x86_64/*.rpm ${GITHUB_WORKSPACE}/rpms/
