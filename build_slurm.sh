44e7c7f823011df71e0188a6ecd4213ebed95eb3
jose_d <jose-d@users.noreply.github.com>
2025-03-26 19:16:01 +0100

    Create build_slurm.sh

---
+++ build_slurm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# fail if anything wrong
+set -e
+
+# print input vars
+echo "SLURM_RELTAG: ${SLURM_RELTAG}"
+
+# enable shell debug
+set -x
+
+# install deps
+dnf -y install ${GITHUB_WORKSPACE}/pmix_rpms/*.rpm
+
+# mkdir for rpmbuild and copy tarball there
+mkdir -p "${HOME}/rpmbuild/SOURCES/"
+cp ${GITHUB_WORKSPACE}/slurm-*.tar.bz2 $HOME/rpmbuild/SOURCES/
+
+# dump rpmlist for possible forensic
+rpm -qa | sort > "${GITHUB_WORKSPACE}/image_slurm_rpms.txt"
+
+# do rpmbuild
+rpmbuild --define '_with_nvml --with-nvml=/usr/local/cuda/targets/x86_64-linux/' \
+          --with pam \
+          --with slurmrestd \
+          --with hwloc \
+          --with lua \
+          --with mysql \
+          --with numa \
+          --with pmix \
+          -ba ./slurm-*/slurm.spec &> ${GITHUB_WORKSPACE}/slurm_build.log
+
+mkdir "${GITHUB_WORKSPACE}/rpms"
+cp ${HOME}/rpmbuild/RPMS/x86_64/slurm-*.rpm "${GITHUB_WORKSPACE}/rpms/"
+
+set +x
