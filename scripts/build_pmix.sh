10ea0210c889449b0ce640a54fc617221b889103
jose_d <jose-d@users.noreply.github.com>
2026-04-24 23:02:50 +0200

    Add '--enable-pmix-binaries' to configure options

--- scripts/build_pmix.sh
+++ scripts/build_pmix.sh
@@ -52,7 +52,7 @@ fi
          --define 'install_modulefile 1' \
          --define "reltag ${PMIX_RELTAG}" \
          --define "opt_prefix_base ${PMIX_OPT_PREFIX_BASE:-/opt/pmix}" \
-         --define "configure_options ${PMIX_CONFIGURE_OPTIONS:---with-tests-examples --disable-per-user-config-files --with-munge=no}" \
+         --define "configure_options ${PMIX_CONFIGURE_OPTIONS:---with-tests-examples --disable-per-user-config-files --with-munge=no --enable-pmix-binaries }" \
          -ba "${PMIX_SPEC_PATH}"
 
 mkdir -p "${GITHUB_WORKSPACE}/rpms"
